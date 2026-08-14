247ae7ffff8e71dff67074cec4dc68c6b7e5d595
Matilde Fonseca <matilde.fonseca@su.se>
2026-08-14 17:13:33 +0200

    Removed warning messages from website.

--- format_data/Format_wikipedia_table.R
+++ format_data/Format_wikipedia_table.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(tidyr)
 
 #Read table extracted from wikipedia
-og_table <- read_csv("Eclipses_21st_century.csv")[-1, ]
+og_table <- read_csv("data/Eclipses_21st_century.csv")[-1, ]
 mod_table <- og_table
 
 #Change date format from Month day, year (ex.: August 13, 2026) to year-month-day (ex.: 2026-08-13)
