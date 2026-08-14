b5c80fbbb3be1f17d05503086fd476e0f6addc51
Andrea <andrea.villanueva@scilifelab.se>
2026-08-14 15:48:53 +0200

    Formatted coordinates in the original csv and created code to make a map with all the 21st century eclipses

--- Format_wikipedia_table.R
+++ Format_wikipedia_table.R
@@ -1,4 +1,6 @@
 library(readr)
+library(dplyr)
+library(tidyr)
 
 #Read table extracted from wikipedia
 og_table <- read_csv("Eclipses_21st_century.csv")[-1, ]
@@ -8,3 +10,23 @@ mod_table <- og_table
 mod_table$Date <- format(as.Date(mod_table$Date, format = "%B %d, %Y"), "%Y-%m-%d")
 
 mod_table <- mod_table[, 1:(ncol(mod_table) - 2)]
+
+#Separate coordinates in three columns
+
+mod_table <- mod_table |>
+  mutate(
+    Location = sub(".*white-space:nowrap\\}", "", Location)
+  ) |>
+  separate(
+    Location,
+    into = c("location-hyphen", "location-degree", "location-numeric"),
+    sep = " / "
+  ) |>
+  separate("location-numeric",
+    into = c("Latitude", "Longitude"),
+    sep = "; "
+  ) |>
+  select(-c("location-hyphen", "location-degree"))
+
+#Save formatted table
+write_csv(mod_table, "formatted_eclipses.csv")
